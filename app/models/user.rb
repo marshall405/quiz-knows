c33ace54727a98f05c79a6a90c05c8c7f835bf32
marshall405 <marshall.slemp@gmail.com>
2020-05-25 20:31:02 -0500

    add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
-
+  validates :password, length: {within: 6..10}
 
   has_many :created_quizzes, foreign_key: :owner_id, class_name: 'Quiz'
 
